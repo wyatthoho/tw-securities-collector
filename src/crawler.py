3bc780c486bbfad42bd02aafa1f6938251e58e0c
Wyatt Ho <wyatthoho@gmail.com>
2023-03-03 15:35:59 +0800

    Refactor: reorder functions in crawler

--- src/crawler.py
+++ src/crawler.py
@@ -5,35 +5,6 @@ import time
 from bs4 import BeautifulSoup
 
 
-def PutStockListTable(table, stockNo, stockName, listedOrOTC, stockType, industry, stockListedDate):
-    condition1 = stockNo[-1].isalpha()
-    condition2 = stockNo[0].isalpha()
-    
-    condition3 = listedOrOTC not in ['上櫃', '期貨及選擇權', '興櫃一般板', '公開發行', '創櫃版']
-    condition4 = stockType in ['ETF', '股票']
-
-    if not (condition1 or condition2) and condition3 and condition4:
-        getThisStock = True
-    else:
-        getThisStock = False
-
-    if getThisStock:
-        if len(table) == 0:
-            table['有價證券代號'] = [stockNo]
-            table['有價證券名稱'] = [stockName]
-            table['有價證券別'] = [stockType]
-            table['產業別'] = [industry]
-            table['發行日'] = [stockListedDate]
-        else:
-            table['有價證券代號'].append(stockNo)
-            table['有價證券名稱'].append(stockName)
-            table['有價證券別'].append(stockType)
-            table['產業別'].append(industry)
-            table['發行日'].append(stockListedDate)
-
-    return table
-
-
 def crawl_companies_etfs():
     '''
     Collect the List of Taiwan Stock Exchange Listed Companies and ETFs.
@@ -69,76 +40,31 @@ def crawl_companies_etfs():
     return pd.DataFrame(table)
 
 
-def crawl_listed_date(stockNo):
-    url = "https://isin.twse.com.tw/isin/single_main.jsp?"
-    payload = {'owncode': str(stockNo), 'stockname': ''}
-    headers = {'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Mobile Safari/537.36'}
-    response = requests.get(url, params=payload, headers=headers)
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    for element in soup.find_all('td'):
-        text = element.get_text()
-        condition1 = len(text.split('/')) == 3
-        condition2 = text.replace('/', '').isdigit()
-        if condition1 and condition2:
-            year, month, day = [int(digit) for digit in text.split('/')]
-            listedDate = datetime.date(year, month, day)
-    return listedDate
-
-
-def CrawlPrice(date, stockNo):
-    url = "https://www.twse.com.tw/exchangeReport/STOCK_DAY"
-    payload = {'response': 'json', 'date': str(date), 'stockNo': str(stockNo)}
-    headers = {'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Mobile Safari/537.36'}
+def PutStockListTable(table, stockNo, stockName, listedOrOTC, stockType, industry, stockListedDate):
+    condition1 = stockNo[-1].isalpha()
+    condition2 = stockNo[0].isalpha()
     
-    try:
-        CheckDateFormat(date)
-    except Exception as e:
-        print(e.args[0])
-        quit()
+    condition3 = listedOrOTC not in ['上櫃', '期貨及選擇權', '興櫃一般板', '公開發行', '創櫃版']
+    condition4 = stockType in ['ETF', '股票']
 
-    response = requests.get(url, params=payload, headers=headers)
-    msg = 'Loading: {}'.format(response.url)
-    print(msg)
-    return eval(response.text)
+    if not (condition1 or condition2) and condition3 and condition4:
+        getThisStock = True
+    else:
+        getThisStock = False
 
-
-def CheckDateFormat(date):
-    if type(date) != str:
-        raise Exception('Date should be input as string')
-    
-    if len(date) != 8:
-        raise Exception('Date should be input as: yyyymmdd')
-    
-    if not date.isdigit():
-        raise Exception('Date should be characters in digits')
-
-
-def CheckDateOrder(yearStr, monthStr, yearEnd, monthEnd):
-    dateStr = datetime.date(yearStr, monthStr, 1)
-    dateEnd = datetime.date(yearEnd, monthEnd, 1)
-
-    if dateStr > dateEnd:
-        raise Exception('The start date exceed the end date')
-
-
-def PutHistoryTable(content, table):
-    fields = content['fields']
-    data = content['data']
-
-    columnNum = len(fields)
-    rowNum = len(data)
-
-    for col in range(columnNum):
-        title = fields[col]
-        
-        for row in range(rowNum):
-            values = data[row][col]
-
-            if title in table.keys():
-                table[title].append(values)
-            else:
-                table[title] = [values]
+    if getThisStock:
+        if len(table) == 0:
+            table['有價證券代號'] = [stockNo]
+            table['有價證券名稱'] = [stockName]
+            table['有價證券別'] = [stockType]
+            table['產業別'] = [industry]
+            table['發行日'] = [stockListedDate]
+        else:
+            table['有價證券代號'].append(stockNo)
+            table['有價證券名稱'].append(stockName)
+            table['有價證券別'].append(stockType)
+            table['產業別'].append(industry)
+            table['發行日'].append(stockListedDate)
 
     return table
 
@@ -183,6 +109,80 @@ def crawl_stock_price(stock_idx):
             break
 
     return pd.DataFrame(table)
+
+
+def crawl_listed_date(stockNo):
+    url = "https://isin.twse.com.tw/isin/single_main.jsp?"
+    payload = {'owncode': str(stockNo), 'stockname': ''}
+    headers = {'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Mobile Safari/537.36'}
+    response = requests.get(url, params=payload, headers=headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    for element in soup.find_all('td'):
+        text = element.get_text()
+        condition1 = len(text.split('/')) == 3
+        condition2 = text.replace('/', '').isdigit()
+        if condition1 and condition2:
+            year, month, day = [int(digit) for digit in text.split('/')]
+            listedDate = datetime.date(year, month, day)
+    return listedDate
+
+
+def CheckDateOrder(yearStr, monthStr, yearEnd, monthEnd):
+    dateStr = datetime.date(yearStr, monthStr, 1)
+    dateEnd = datetime.date(yearEnd, monthEnd, 1)
+
+    if dateStr > dateEnd:
+        raise Exception('The start date exceed the end date')
+
+
+def CrawlPrice(date, stockNo):
+    url = "https://www.twse.com.tw/exchangeReport/STOCK_DAY"
+    payload = {'response': 'json', 'date': str(date), 'stockNo': str(stockNo)}
+    headers = {'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Mobile Safari/537.36'}
+    
+    try:
+        CheckDateFormat(date)
+    except Exception as e:
+        print(e.args[0])
+        quit()
+
+    response = requests.get(url, params=payload, headers=headers)
+    msg = 'Loading: {}'.format(response.url)
+    print(msg)
+    return eval(response.text)
+
+
+def CheckDateFormat(date):
+    if type(date) != str:
+        raise Exception('Date should be input as string')
+    
+    if len(date) != 8:
+        raise Exception('Date should be input as: yyyymmdd')
+    
+    if not date.isdigit():
+        raise Exception('Date should be characters in digits')
+
+
+def PutHistoryTable(content, table):
+    fields = content['fields']
+    data = content['data']
+
+    columnNum = len(fields)
+    rowNum = len(data)
+
+    for col in range(columnNum):
+        title = fields[col]
+        
+        for row in range(rowNum):
+            values = data[row][col]
+
+            if title in table.keys():
+                table[title].append(values)
+            else:
+                table[title] = [values]
+
+    return table
 
 
 if __name__ == '__main__':
