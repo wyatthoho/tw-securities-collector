9a7a277d7bf8c9afb739808f8ccac6770953ee6d
Wyatt Ho <wyatthoho@gmail.com>
2023-04-19 15:15:04 +0800

    chore: rename functions

--- src/main.py
+++ src/main.py
@@ -5,13 +5,13 @@ import crawler
 import crud_mongo
 
 
-def get_docs(df: pandas.DataFrame) -> List[Dict]:
+def transform_df(df: pandas.DataFrame) -> List[Dict]:
     return [row.to_dict() for idx, row in df.iterrows()]
 
 
 def main():
     companies_etfs = crawler.get_companies_etfs()
-    docs = get_docs(companies_etfs)
+    docs = transform_df(companies_etfs)
     crud_mongo.update_docs(
         db_name='taiwan_stocks',
         collection_name='companies_etfs',
@@ -22,7 +22,7 @@ def main():
         stock_name = row['有價證券名稱']
         stock_idx = row['有價證券代號']
         stock_history = crawler.get_stock_prices(stock_idx)
-        docs = get_docs(stock_history)
+        docs = transform_df(stock_history)
         crud_mongo.update_docs(
             db_name='taiwan_stocks',
             collection_name=stock_name,
